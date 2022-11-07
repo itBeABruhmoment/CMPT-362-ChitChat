7e0b8283b992b9fd65f0f4f5709382dbb51e6831
hvtruong <truonghoangvietthv@gmail.com>
2022-11-06 21:22:20 -0800

    Initial commit

---
+++ app/src/main/java/com/example/cmpt_362_chitchat/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.cmpt_362_chitchat.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
