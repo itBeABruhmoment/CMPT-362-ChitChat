33e06e5b5f4bb9f95026053f4c05802546c30954
Maxwell Zhang <mtz3@sfu.ca>
2022-11-15 09:08:44 -0800

    set up friend request fragment

---
+++ app/src/main/java/com/example/cmpt_362_chitchat/ui/friends/FriendRequest.kt
@@ -0,0 +1,10 @@
+package com.example.cmpt_362_chitchat.ui.friends
+
+// temp class for showing some working UI
+class FriendRequest {
+    var name: String = ""
+
+    constructor(name: String) {
+        this.name = name
+    }
+}
--- app/src/main/java/com/example/cmpt_362_chitchat/ui/friends/ViewFriendRequestsFragment.kt
+++ app/src/main/java/com/example/cmpt_362_chitchat/ui/friends/ViewFriendRequestsFragment.kt
@@ -1,9 +1,11 @@
 package com.example.cmpt_362_chitchat.ui.friends
 
+import android.app.DownloadManager.Request
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.example.cmpt_362_chitchat.R
 
@@ -13,6 +15,13 @@ class ViewFriendRequestsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_view_friend_requests, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_view_friend_requests, container, false)
+
+        val requests: ArrayList<FriendRequest> = arrayListOf(FriendRequest("A"), FriendRequest("B"), FriendRequest("C"))
+        val listView: ListView = view.findViewById(R.id.fragment_view_friend_requests_list)
+        val adapter: FriendRequestArrayAdapter = FriendRequestArrayAdapter(requests, requireContext())
+        listView.adapter = adapter
+
+        return view
     }
 }
