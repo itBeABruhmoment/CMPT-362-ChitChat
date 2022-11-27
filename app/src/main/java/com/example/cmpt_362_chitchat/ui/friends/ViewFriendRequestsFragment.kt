9f9989abf908a2da0df87d2d2ca1f766e7576972
Maxwell Zhang <mtz3@sfu.ca>
2022-11-27 15:11:50 -0800

    integrate viewmodel

--- app/src/main/java/com/example/cmpt_362_chitchat/ui/friends/ViewFriendRequestsFragment.kt
+++ app/src/main/java/com/example/cmpt_362_chitchat/ui/friends/ViewFriendRequestsFragment.kt
@@ -3,6 +3,7 @@ package com.example.cmpt_362_chitchat.ui.friends
 import android.content.Intent
 import android.os.Bundle
 import android.provider.ContactsContract.Data
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,7 +22,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class ViewFriendRequestsFragment : Fragment() {
-    //private lateinit var viewModel: FriendsActivityViewModel
+    private lateinit var viewModel: FriendsActivityViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,7 +31,17 @@ class ViewFriendRequestsFragment : Fragment() {
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_view_friend_requests, container, false)
 
-        //viewModel = ViewModelProvider(requireActivity()).get(FriendsActivityViewModel::class.java)
+        // get viewmodel
+        val tempUser: FirebaseUser? = Firebase.auth.currentUser
+        if(tempUser == null) {
+            Log.i("FriendsActivity", "user null, going to login page")
+            val intent: Intent = Intent(requireActivity(), LoginActivity::class.java)
+            startActivity(intent)
+        } else {
+            Log.i("FriendsActivity", "user not null, continue")
+            val viewModelFactory: FriendsActivityViewModelFactory = FriendsActivityViewModelFactory(tempUser)
+            viewModel = ViewModelProvider(requireActivity(), viewModelFactory).get(FriendsActivityViewModel::class.java)
+        }
 
         val requests: ArrayList<User> = arrayListOf(User("Mai"), User("Oliver"), User("Harry"))
         val listView: ListView = view.findViewById(R.id.fragment_view_friend_requests_list)
