06315699b6210c3ed61091a775010e07492c701c
hvtruong <truonghoangvietthv@gmail.com>
2022-12-04 23:20:51 -0800

    Added image/gif insertion to chat box and displayed in chat room

--- app/src/main/java/com/example/cmpt_362_chitchat/data/Message.kt
+++ app/src/main/java/com/example/cmpt_362_chitchat/data/Message.kt
@@ -3,9 +3,16 @@ package com.example.cmpt_362_chitchat.data
 class Message() {
     var message: String? = null
     var sendID: String? = null
+    var imageName: String? = ""
 
     constructor(message: String?, sendID: String?) : this() {
         this.message = message
         this.sendID = sendID
+    }
+
+    constructor(message: String?, sendID: String?, imageName: String?) : this() {
+        this.message = message
+        this.sendID = sendID
+        this.imageName = imageName
     }
 }
